edc5964f0474f09a5b12a6963d75237783e88393
mphelps1978 <phelps.michael@live.com>
2020-01-07 12:06:47 -0600

    Hooked Login Form to Login Component on app.js

--- gigapet/src/App.js
+++ gigapet/src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Router, Route, Link, Switch } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 import UpdateForm from "./components/UpdateForm";
 // import Parent from "./components/Parent";
 import { createBrowserHistory } from "history";
-import './App.css';
+import "./App.css";
 
 const history = createBrowserHistory();
 function App() {
@@ -14,27 +15,42 @@ function App() {
   const addToSaveList = user => {
     setSaveList([...saveList, user]);
   };
-  const updateUsers = (user) => {
-    console.log(user)
-    setUsers({ users: [users, user] })
-  }
+  const updateUsers = user => {
+    console.log(user);
+    setUsers({ users: [users, user] });
+  };
   // const removeItem = id =>{
   //   setChildren(children.filter(item =>item.id !== id))
   // }
   return (
-
     <Router history={history}>
       <div className="App">
         <ul>
-          <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/auth/:id">Update Form</Link></li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/auth/:id">Update Form</Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
           {/* <li><Link to="protected">Parent</Link></li> */}
         </ul>
         <Route exact path="/" component={SignUp} />
-        <Route path={`/auth/:id`} render={props => {
-          return <UpdateForm {...props} history={history} updateUsers={updateUsers} />
-        }}
+        <Route
+          path={`/auth/:id`}
+          render={props => {
+            return (
+              <UpdateForm
+                {...props}
+                history={history}
+                updateUsers={updateUsers}
+              />
+            );
+          }}
         />
+        <Route path={"/login"} component={Login} />
 
         {/* <Switch>
           <PrivateRoute path="/protected" component={Parent} />
@@ -42,8 +58,6 @@ function App() {
           <Route path="/signup" component={SignUp} />
 
         </Switch> */}
-
-
       </div>
     </Router>
   );
