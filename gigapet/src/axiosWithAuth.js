4bad02aaf0c59f5abe6753e55a50fc788f7d7410
Nerissa Taylor <taylornerissa@hotmail.com>
2020-01-07 17:46:56 -0500

    merge

--- gigapet/src/axiosWithAuth.js
+++ gigapet/src/axiosWithAuth.js
@@ -2,11 +2,7 @@ import axios from 'axios';
 const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
     return axios.create({
-<<<<<<< HEAD
-        baseUrl: "https://build-gigapet.herokuapp.com/api",
-=======
         baseUrl: 'https://build-gigapet.herokuapp.com',
->>>>>>> a5e4912e28679851260674e2903c0afa74ea4390
         headers: {
             'Authorization': token,
             'Content-Type': 'application/json'
